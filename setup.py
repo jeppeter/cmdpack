6c3ae9bb126fc06b914b2b37b9736c772a40df97
jeppeter <jeppeter@gmail.com>
2016-07-20 16:57:02 +0800

    to make cmdpack ok

--- setup.py
+++ setup.py
@@ -2,16 +2,12 @@
 from setuptools import setup
 
 setup(name='cmdpack',
-	version='0.1.0',
+	version='0.1.4',
 	description='command call back filter package',
 	url='http://github.com/jeppeter/cmdpack',
 	author='jeppeter Wang',
 	author_email='jeppeter@gmail.com',
 	license='MIT',
 	packages=['cmdpack'],
-	install_requires=[
-		'tempfile',
-		'unittest'
-	],
 	keyword = ['commandline'],
 	zip_safe=False)
